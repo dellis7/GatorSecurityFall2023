612755cbf69c579040b1ca33a5ea5c8c3ae69eb6
Dylan Tosh <dylantosh@ufl.edu>
2023-01-28 10:22:11 -0600

    Fixed Navbar.js Issues causing Console Errors

--- senior_project_dea-main/src/components/Navbar.js
+++ senior_project_dea-main/src/components/Navbar.js
@@ -48,15 +48,41 @@ const navbarStyle = {
   return (
     <Navbar style={navbarStyle} expand="lg">
       <Container>
-        <LinkContainer to="/welcome" style={navbarTitle}><Navbar.Brand style={navbarTitle}>Gator Security Fundamentals</Navbar.Brand></LinkContainer>
+        <LinkContainer to="/welcome" style={navbarTitle}>
+          <Navbar.Brand style={navbarTitle}>
+            Gator Security Fundamentals
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav class="navbar-nav ms-auto mb-2 mb-lg-0" style={{color:'white'}}>            
-            <LinkContainer to="/learn" style={navLink}><Nav.Link style={navLink} eventKey={1}> <div style={linkContainer}><div>Learn</div><img src='./bookIcon.png' alt=''/></div></Nav.Link></LinkContainer>                 
-            <LinkContainer to="/game" style={navLink}><Nav.Link style={navLink} eventKey={2} ><div style={linkContainer}><div>Game</div><img src='./gameIcon.png' alt=''/></div></Nav.Link></LinkContainer>
-            <NavDropdown style={dropdown} eventKey={3} title={<img src='./profileIcon.png' alt=''/>}>
-                <LinkContainer to="/myprofile" style={dropdownItem}><NavDropdown.Item style={dropdownItem} eventKey={3.1}>My Profile</NavDropdown.Item></LinkContainer>
-                <LinkContainer to="/log-out" style={dropdownItem}><NavDropdown.Item style={dropdownItem} eventKey={3.2}>Logout</NavDropdown.Item></LinkContainer>
+          <Nav className="navbar-nav ms-auto mb-2 mb-lg-0" style={{color:'white'}}>            
+            <LinkContainer to="/learn" style={navLink}>
+              <Nav.Link style={navLink} eventKey={1}> 
+                <div style={linkContainer}>
+                  Learn
+                  <img src='./bookIcon.png' alt=''/>
+                </div>
+              </Nav.Link>
+            </LinkContainer>                 
+            <LinkContainer to="/game" style={navLink}>
+              <Nav.Link style={navLink} eventKey={2}>
+                <div style={linkContainer}>
+                  Game
+                  <img src='./gameIcon.png' alt=''/>
+                </div>
+              </Nav.Link>
+            </LinkContainer>
+            <NavDropdown style={dropdown} title={<img src='./profileIcon.png' alt=''/>}>
+              <LinkContainer to="/myprofile" style={dropdownItem}>
+                <NavDropdown.Item style={dropdownItem} eventKey={3.1}>
+                  My Profile
+                </NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/log-out" style={dropdownItem}>
+                <NavDropdown.Item style={dropdownItem} eventKey={3.2}>
+                  Logout
+                </NavDropdown.Item> 
+              </LinkContainer>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
