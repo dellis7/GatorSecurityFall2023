b5fc6183558f0da2f2b18cfa926adf3345937c84
Dylan Tosh <dylantosh@ufl.edu>
2023-03-31 16:54:12 -0500

    Added scoring to MM games/Fixed font bug on Games page

--- senior_project_dea-main/src/components/questions/Game.js
+++ senior_project_dea-main/src/components/questions/Game.js
@@ -106,13 +106,13 @@ function GamePage() {
                     <p className="card-text">
                         Select a choose your own adventure game to play below.
                         <br></br>
-                        (Photo by <a href="https://www.pexels.com/@pixabay/">Pixabay</a> on <a href="https://www.pexels.com/photo/blur-bright-business-codes-207580/" className='link-text'>Pexels)</a>
+                        (Photo by <a href="https://www.pexels.com/@pixabay/" className='link-text'>Pixabay</a> on <a href="https://www.pexels.com/photo/blur-bright-business-codes-207580/" className='link-text'>Pexels)</a>
                     </p>
                     {cyoaQuestionDisplay}
                 </div>
             </div>
             <div className='card game'>
-                <img src="./security-4868165_1920.jpg" className='img-size' alt="Neon Lock"/>
+                <img src="./security-4868165_1920.jpg" className='img-size' alt="Cyber Lock"/>
                 <div className="card-body">
                     <h5 style={{"fontWeight": 'bold'}}>
                         Drag and Drop Games
@@ -120,8 +120,7 @@ function GamePage() {
                     <p className="card-text">
                         Select a drag and drop game to play below.
                         <br></br>
-                        {/*(Photo by Professor Resch (This is a template))*/}
-                        (Photo by <a href="https://pixabay.com/users/thedigitalartist-202249/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4868165" className='link-text'>Pete Linforth</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4868165" className='link-test'>Pixabay</a>)
+                        (Photo by <a href="https://pixabay.com/users/thedigitalartist-202249/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4868165" className='link-text'>Pete Linforth</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4868165" className='link-text'>Pixabay)</a>
                     </p>
                     {dndQuestionDisplay}
                 </div>
@@ -135,13 +134,9 @@ function GamePage() {
                     <p className="card-text">
                         Select a memory matching card game to play below.
                         <br></br>
-                        (Photo by <a href="https://pixabay.com/users/geralt-9301/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3706562" className='link-text'>Gerd Altmann</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3706562" className='link-text'>Pixabay</a>)
-                        
+                        (Photo by <a href="https://pixabay.com/users/geralt-9301/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3706562" className='link-text'>Gerd Altmann</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3706562" className='link-text'>Pixabay)</a>  
                     </p>
                     {matchingQuestionDisplay}
-                    {/* <a href="./gameMatching" className="btn btn-primary">
-                        Cybersecurity Terms and Definitions
-                    </a> */}
                 </div>
             </div>
             <div className='card game'>
