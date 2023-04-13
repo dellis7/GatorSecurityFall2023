937f96204fb63f0bf211d5991c1aa015bfc92e29
cwojtak <connor.wojtak@gmail.com>
2023-04-13 09:34:40 -0500

    Test carousel

--- senior_project_dea-main/src/components/Welcome.js
+++ senior_project_dea-main/src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import GetConfig from '../Config.js';
-import {MDBCarousel, MDBCarouselItem} from 'mdb-react-ui-kit';
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class WelcomePage extends React.Component {
   constructor(props){
@@ -56,47 +56,39 @@ export default class WelcomePage extends React.Component {
     let name = this.state.userInfo["fname"];
 
   return (
-    <MDBCarousel showControls showIndicators style={carousel} interval={8000}>
-      <MDBCarouselItem        
+    <Carousel style={carousel} interval={8000}>
+      <Carousel.Item   
         className='w-100 d-block'
         itemId={1}
         src='./Orange-Background.png'
         alt='...'
       >
-        <img src='./welcomeImg.png' style={image} alt="Stick Figure Waving"></img>
-        <h5 style={title}>Welcome {name}!</h5>
-        <p style={caption}>Please navigate to the "Learn" page to read up on important topics, and then head over to the "Game" page to test your knowledge.</p>
-      </MDBCarouselItem>
-      <MDBCarouselItem
+        <img src='./1.png' style={image} alt="Stick Figure Waving"></img>
+      </Carousel.Item>
+      <Carousel.Item
         className='w-100 d-block'
         itemId={2}
         src='./Orange-Background.png'
         alt='...'
       >
-        <img src='./scoreImg.png' style={image} alt="Ascending Steps with Flag on Last Step"></img>
-        <h5 style={title}>Checking Your Score</h5>
-        <p style={caption}>Take a look at your "My Profile" page to see your progress. There you can see how many sections and game questions you have completed. Keep up the great work!</p>
-      </MDBCarouselItem>
-      <MDBCarouselItem        
+        <img src='./2.png' style={image} alt="Ascending Steps with Flag on Last Step"></img>
+      </Carousel.Item>
+      <Carousel.Item        
         className='w-100 d-block'
         itemId={3}
         src='./Orange-Background.png'
         alt='...'
       >
-        <img src='./creatorImg.png' style={image} alt="Lightbulb"></img>
-        <h5 style={title}>The Creators</h5>
-        <p style={caption}>This website was created by University of Florida students Daymao Silva, Erick Gonzalez, and Annalina Becker for their Fall 2022 senior project. They were advised under Professor Cheryl Resch.</p>
-      </MDBCarouselItem>
-      <MDBCarouselItem        
+        <img src='./3.png' style={image} alt="Lightbulb"></img>
+      </Carousel.Item>
+      <Carousel.Item     
         className='w-100 d-block'
         itemId={3}
         src='./Orange-Background.png'
         alt='...'
       >
-        <img src='./creatorImg.png' style={image} alt="Lightbulb"></img>
-        <h5 style={title}>The Creators</h5>
-        <p style={caption}>Development of this website was continued in Spring 2023 by University of Florida seniors Kerry Hannigan, Dylan Tosh, Connor Wojtak, Jacob Boney, and Brian Hoblin under Professor Cheryl Resch.</p>
-      </MDBCarouselItem>
-    </MDBCarousel>
+        <img src='./4.png' style={image} alt="Lightbulb"></img>
+      </Carousel.Item>
+    </Carousel>
   );
 }}
