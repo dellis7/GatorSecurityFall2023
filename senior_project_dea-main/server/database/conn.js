dea5a610001324f05f3461c48ba0ee7f31ba3b0d
Deven <dellis1@ufl.edu>
2023-09-05 13:09:49 -0400

    edited conn.js to connect to our mongodb

--- senior_project_dea-main/server/database/conn.js
+++ senior_project_dea-main/server/database/conn.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv")
 
 //Prepare environment file and connection string
 dotenv.config('./.env')
-const mongoUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pfxgixu.mongodb.net/?retryWrites=true&w=majority`
+//const mongoUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pfxgixu.mongodb.net/?retryWrites=true&w=majority`
+const mongoUrl = `mongodb+srv://admin:admin@cluster0.haqdv9a.mongodb.net/?retryWrites=true&w=majority`
 
 //Perform connection
 const connectDb = async() =>
