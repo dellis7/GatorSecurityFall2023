cd7b2c90e10e2d28e30ac03614313aa488ea7497
Deven <dellis1@ufl.edu>
2023-09-29 10:19:05 -0400

    added scripts to start the app

--- senior_project_dea-main/src/Config.js
+++ senior_project_dea-main/src/Config.js
@@ -1,7 +1,7 @@
 //To change the address of the machine hosting the backend, modify line 5 below.
 function GetConfig() {
     return {
-        "SERVER_ADDRESS": "http://localhost:5000"
+        "SERVER_ADDRESS": "http://75.3.229.175:5000"
     };
 }
 
