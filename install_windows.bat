9708fefd6be66839a68b7a5aa8863075a935ef48
Deven <dellis1@ufl.edu>
2023-10-27 11:34:58 -0400

    added an install script for linux

--- install_windows.bat
+++ install_windows.bat
@@ -4,7 +4,6 @@ npm i mdb-react-ui-kit
 cd server
 npm install
 npm install cors
-touch .env
 echo $'DB_URI=EDIT_ME\nDB_USERNAME=EDIT_ME\nDB_PASSWORD=EDIT_ME\nJWT_SECRET=EDIT_ME\nLPORT=5000' >.env
 echo 'EDIT .env FILE"
 cd ..
